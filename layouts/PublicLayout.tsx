84b9926185a39aee04a6248f1524e58f80b2de60
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-16 00:15:20 +0530

    chore: static changes

---
+++ layouts/PublicLayout.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/layouts/Footer';
+import Navbar from '@/components/layouts/Navbar';
+import React from 'react';
+
+const PublicLayout = () => {
+	return (
+		<>
+			<Navbar />
+			<Footer />
+		</>
+	);
+};
+
+export default PublicLayout;
